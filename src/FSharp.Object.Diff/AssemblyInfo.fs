b4a474acff728194f7dc66fd497b7c8f60b9f292
pocketberserker <pocketberserker@gmail.com>
2016-03-13 00:00:29 +0900

    init commit

---
+++ src/FSharp.Object.Diff/AssemblyInfo.fs
@@ -0,0 +1,15 @@
+﻿namespace System
+open System.Reflection
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitleAttribute("FSharp.Object.Diff")>]
+[<assembly: AssemblyProductAttribute("FSharp.Obejct.Diff")>]
+[<assembly: GuidAttribute("aac4b74e-0af4-459a-ba59-2a5788e3675b")>]
+[<assembly: AssemblyDescriptionAttribute("")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.1.0"
