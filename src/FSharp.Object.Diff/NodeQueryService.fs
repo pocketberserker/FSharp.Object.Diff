ac74747e77c8a0483bfba8648d2c41fd4ee1572e
pocketberserker <pocketberserker@gmail.com>
2016-03-20 03:00:22 +0900

    NodeQueryService

---
+++ src/FSharp.Object.Diff/NodeQueryService.fs
@@ -0,0 +1,26 @@
+﻿namespace FSharp.Object.Diff
+
+type NodeQueryService =
+  inherit CategoryResolver
+  inherit IsIntrospectableResolver
+  inherit IsIgnoredResolver
+  inherit IsReturnableResolver
+  inherit ComparisonStrategyResolver
+  inherit PrimitiveDefaultValueModeResolver
+
+type DefaultNodeQueryService(
+                             categoryResolver: CategoryResolver,
+                             introspectableResolver: IsIntrospectableResolver,
+                             ignoredResolver: IsIgnoredResolver,
+                             returnableResolver: IsReturnableResolver,
+                             comparisonStrategyResolver: ComparisonStrategyResolver,
+                             primitiveDefaultValueModeResolver: PrimitiveDefaultValueModeResolver
+  ) =
+
+  interface NodeQueryService with
+    member __.ResolveCategories(node) = categoryResolver.ResolveCategories(node)
+    member __.IsIntrospectable(node) = introspectableResolver.IsIntrospectable(node)
+    member __.IsIgnored(node) = ignoredResolver.IsIgnored(node)
+    member __.IsReturnable(node) = returnableResolver.IsReturnable(node)
+    member __.ResolveComparisonStrategy(node) = comparisonStrategyResolver.ResolveComparisonStrategy(node)
+    member __.ResolvePrimitiveDefaultValueMode(node) = primitiveDefaultValueModeResolver.ResolvePrimitiveDefaultValueMode(node)
