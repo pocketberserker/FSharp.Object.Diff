1b5cf000e807ddacd0df0f5429e498c4211ca6a5
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2016-03-22 17:41:05 +0900

    add red test

---
+++ tests/FSharp.Object.Diff.Tests/StringsTest.fs
@@ -0,0 +1,22 @@
+﻿module StringsTest
+
+open Persimmon
+open UseTestNameByReflection
+
+open FSharp.Object.Diff
+
+let ``toSingleLineString`` () =
+  let test (input, expected) = test {
+    let res = String.toSingleLineString input
+    do! assertEquals expected res
+  }
+  parameterize {
+    source [
+      (null, null)
+      ("", "")
+      (" aaa ", "aaa")
+      (" aaa\nbbb ", @"aaa \\ bbb")
+      (" aaa \n bbb ", @"aaa \\ bbb")
+    ]
+    run test
+  }
