0670bd0293df9eba52fd136784e82dc66cdf3a63
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2016-03-22 17:42:55 +0900

    fix toSingleLineString

--- src/FSharp.Object.Diff/Strings.fs
+++ src/FSharp.Object.Diff/Strings.fs
@@ -1,13 +1,14 @@
 ﻿module FSharp.Object.Diff.String
 
-open System.Text
+open System.Text.RegularExpressions
 
 [<Literal>]
-let LineBreakPattern = "\\s*\\n\\s*"
+let LineBreakPattern = @"\s*\n\s*"
 
 let toSingleLineString (o: obj) =
-  if o <> null then
-    o.ToString().Trim().Replace(LineBreakPattern, " \\\\ ")
-  else null
+  match o with
+  | null -> null
+  | notNull ->
+      Regex.Replace(notNull.ToString().Trim(), LineBreakPattern, @" \\ ")
 
 let indent times text = String.replicate times "  " + text
