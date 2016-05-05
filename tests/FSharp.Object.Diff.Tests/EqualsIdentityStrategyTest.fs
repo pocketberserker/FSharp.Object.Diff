fe8b1196f02e81eb62be8e738cc4fa9b3b0720f5
pocketberserker <pocketberserker@gmail.com>
2016-05-05 18:36:43 +0900

    add EqualsIdentityStrategy test

---
+++ tests/FSharp.Object.Diff.Tests/EqualsIdentityStrategyTest.fs
@@ -0,0 +1,17 @@
+﻿module FSharp.Object.Diff.Tests.EqualsIdentityStrategyTest
+
+open Persimmon
+open UseTestNameByReflection
+open FSharp.Object.Diff
+
+let equal = parameterize {
+  source [
+    (null, null, true)
+    (null, "foo", false)
+    ("foo", "bar", false)
+    ("foo", "foo", true)
+  ]
+  run (fun (a, b, equal) -> test {
+    do! assertEquals equal (EqualsIdentityStrategy.IsEqual(a, b))
+  })
+}
