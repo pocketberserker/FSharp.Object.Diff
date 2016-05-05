83dca1feaf6904990cc43790f68eafde700635d6
pocketberserker <pocketberserker@gmail.com>
2016-05-05 17:10:08 +0900

    add Category and NpdePathValueHolder tests

---
+++ tests/FSharp.Object.Diff.Tests/CategoryTest.fs
@@ -0,0 +1,87 @@
+﻿module FSharp.Object.Diff.Tests.CategoryServiceTest
+
+open System
+open Persimmon
+open UseTestNameByReflection
+open FSharp.Object.Diff
+open Foq
+
+type Alliance = Alliance
+
+type Target = {
+  CategoryService: CategoryService
+  Accessor: PropertyAwareAccessor
+  NodePath: NodePath
+  NodeType: Type
+  Node: DiffNode
+  RootNode: DiffNode
+}
+
+
+let setup accessor =
+  let nodeType = typeof<Alliance>
+  let rootNode = DiffNode.NewRootNode()
+  {
+    CategoryService = CategoryService(ObjectDifferBuilder())
+    Accessor = accessor
+    NodePath = NodePath.With("foo")
+    NodeType = nodeType
+    RootNode = rootNode
+    Node = DiffNode(rootNode, accessor, nodeType)
+  }
+
+let setupWithDefaultAccessor () =
+  Mock<PropertyAwareAccessor>()
+    .Setup(fun x -> <@ x.ElementSelector @>).Returns(BeanPropertyElementSelector("foo"))
+    .SetupMethod(fun x -> <@ x.GetCategoriesFromAttribute @>).Returns(Set.empty)
+    .Create()
+  |> setup
+
+let ``resolveCategories: should return categories configured via path`` = test {
+  let target = setupWithDefaultAccessor ()
+  target.CategoryService.OfNode(target.NodePath).ToBe([|"Stark"; "Lannister"|]) |> ignore
+  do! assertEquals (Set.ofList ["Stark"; "Lannister"]) (target.CategoryService.ResolveCategories(target.Node))
+}
+
+let ``resolveCategories: should return categories configured via type`` = test {
+  let target = setupWithDefaultAccessor ()
+  target.CategoryService.OfType(target.NodeType).ToBe([|"Stark"; "Lannister"|]) |> ignore
+  do! assertEquals (Set.ofList ["Stark"; "Lannister"]) (target.CategoryService.ResolveCategories(target.Node))
+}
+
+let ``resolveCategories: should return categories configured via node`` = test {
+  let target =
+    Mock<PropertyAwareAccessor>()
+      .Setup(fun x -> <@ x.ElementSelector @>).Returns(BeanPropertyElementSelector("foo"))
+      .SetupMethod(fun x -> <@ x.GetCategoriesFromAttribute @>).Returns(Set.ofList ["Stark"; "Lannister"])
+      .Create()
+    |> setup
+  do! assertEquals (Set.ofList ["Stark"; "Lannister"]) (target.CategoryService.ResolveCategories(target.Node))
+}
+
+let ``resolveCategories: should return combined categories configured via all possible options`` = test {
+  let target =
+    Mock<PropertyAwareAccessor>()
+      .Setup(fun x -> <@ x.ElementSelector @>).Returns(BeanPropertyElementSelector("foo"))
+      .SetupMethod(fun x -> <@ x.GetCategoriesFromAttribute @>).Returns(Set.ofList ["C"])
+      .Create()
+    |> setup
+  target.CategoryService.OfNode(target.NodePath).ToBe([|"A"|]) |> ignore
+  target.CategoryService.OfType(target.NodeType).ToBe([|"B"|]) |> ignore
+  do! assertEquals (Set.ofList ["A"; "B"; "C"]) (target.CategoryService.ResolveCategories(target.Node)) 
+}
+
+let ``resolveCategories: should also return categories of parent nodes`` = test {
+  let target =
+    Mock<PropertyAwareAccessor>()
+      .Setup(fun x -> <@ x.ElementSelector @>).Returns(BeanPropertyElementSelector("foo"))
+      .SetupMethod(fun x -> <@ x.GetCategoriesFromAttribute @>).Returns(Set.ofList ["B"])
+      .Create()
+    |> setup
+  target.CategoryService.OfNode(NodePath.WithRoot()).ToBe([|"A"|]) |> ignore
+  do! assertEquals (Set.ofList ["A"; "B"]) (target.CategoryService.ResolveCategories(target.Node)) 
+}
+
+let ``resolveCategories: should return empty Set if no category is defined`` = test {
+  do! assertEquals Set.empty ((CategoryService(ObjectDifferBuilder())).ResolveCategories(DiffNode.NewRootNode()) )
+}
---
+++ tests/FSharp.Object.Diff.Tests/NodePathValueHolderTest.fs
@@ -0,0 +1,33 @@
+﻿module  FSharp.Object.Diff.Tests.NodePathValueHolderTest
+
+open Persimmon
+open UseTestNameByReflection
+open FSharp.Object.Diff
+
+let ``should store value at node path`` = parameterize {
+  source [
+    (NodePath.WithRoot(), "foo1")
+    (NodePath.With("a"), "foo2")
+    (NodePath.With("a", "b"), "foo3")
+  ]
+  run (fun (nodePath, value) -> test {
+    let valueHolder = NodePathValueHolder<string>()
+    valueHolder.Put(nodePath, Some value) |> ignore
+    do! assertEquals (Some value) (valueHolder.ValueForNodePath(nodePath))
+  })
+}
+
+let ``should return null for unknown paths`` = test {
+  let valueHolder = NodePathValueHolder<string>()
+  do! valueHolder.ValueForNodePath(NodePath.With("a"))
+    |> assertEquals None
+}
+
+let ``should return accumulated values along path`` = test {
+  let valueHolder = NodePathValueHolder<string>()
+  valueHolder.Put(NodePath.WithRoot(), Some "foo1") |> ignore
+  valueHolder.Put(NodePath.With("a"), Some "foo2") |> ignore
+  valueHolder.Put(NodePath.With("a", "b"), Some "foo3") |> ignore
+  do! valueHolder.AccumulatedValuesForNodePath(NodePath.With("a", "b"))
+    |> assertEquals ["foo1"; "foo2"; "foo3"]
+}
