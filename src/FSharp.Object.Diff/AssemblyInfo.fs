94357b71938d55533c1bd94cfa540a2b7d5bea85
pocketberserker <pocketberserker@gmail.com>
2016-05-17 05:35:18 +0900

    Bump version to 0.1.1

--- src/FSharp.Object.Diff/AssemblyInfo.fs
+++ src/FSharp.Object.Diff/AssemblyInfo.fs
@@ -6,11 +6,11 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyProductAttribute("FSharp.Obejct.Diff")>]
 [<assembly: GuidAttribute("aac4b74e-0af4-459a-ba59-2a5788e3675b")>]
 [<assembly: AssemblyDescriptionAttribute("")>]
-[<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
+[<assembly: AssemblyVersionAttribute("0.1.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.1")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.1.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.0"
-    let [<Literal>] InformationalVersion = "0.1.0"
+    let [<Literal>] Version = "0.1.1"
+    let [<Literal>] InformationalVersion = "0.1.1"
